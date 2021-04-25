5ba1cfff97faec0f2712f51aaadcdfb83d518a0d
Moise Rebatta <moiserebatta@gmail.com>
2021-04-25 02:02:52 -0300

    refactor name terminator folder

--- symbolicLinks.sh
+++ symbolicLinks.sh
@@ -4,5 +4,5 @@ cd && \
 ln -s ~/configs/.p10k.zsh .p10k.zsh && \
 ln -s ~/configs/.zshrc .zshrc && \
 cd .config && \
-ln -s ~/configs/terminatorConfig terminator && \
+ln -s ~/configs/terminator terminator && \
 ln -s ~/configs/nvim nvim
